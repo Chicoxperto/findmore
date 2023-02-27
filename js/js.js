dd6c617934627c274b2a2af6bf3713fac309ad2c
Chicoxperto <fhenriques5454@gmail.com>
2023-02-27 21:52:17 +0000

    multiplas alteraçoes - final1

--- js/js.js
+++ js/js.js
@@ -1,14 +1,33 @@
-$(document).ready(function() {
-    $("#show_hide_password a").on('click', function(event) {
-        event.preventDefault();
-        if($('#show_hide_password input').attr("type") == "text"){
-            $('#show_hide_password input').attr('type', 'password');
-            $('#show_hide_password i').addClass( "fa-eye-slash" );
-            $('#show_hide_password i').removeClass( "fa-eye" );
-        }else if($('#show_hide_password input').attr("type") == "password"){
-            $('#show_hide_password input').attr('type', 'text');
-            $('#show_hide_password i').removeClass( "fa-eye-slash" );
-            $('#show_hide_password i').addClass( "fa-eye" );
-        }
-    });
-});
+var imgmenu = 0;
+
+function OpenSideMenu() {
+  var sidemenu = document.getElementById("side-menu-func");
+  sidemenu.classList.toggle("side-menu-active");
+
+  var imgsidemenu = document.getElementById("img-sidemenu");
+  imgsidemenu.classList.toggle("logo-active");
+
+  if (imgmenu == 0) {
+    document.getElementById("img-sidemenu").src =
+      "assets/ico/logo-sidemenu.svg";
+
+    imgmenu++;
+  } else {
+    document.getElementById("img-sidemenu").src = "assets/ico/logo-mini.svg";
+
+    imgmenu = 0;
+  }
+
+  var iconsidemenu = document.getElementById("btn-sidemenu");
+  iconsidemenu.classList.toggle("iconrotation");
+}
+
+function OpenSideMenuMobile() {
+  var sidemenumobile = document.getElementById("side-menu-mob");
+  sidemenumobile.classList.add("active");
+}
+
+function CloseSideMenuMobile() {
+  var sidemenumobileclose = document.getElementById("side-menu-mob");
+  sidemenumobileclose.classList.remove("active");
+}
